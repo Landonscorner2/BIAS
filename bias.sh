d506a2d3ced79154f7e32f9ddf70579b9ce602ff
Landonscorner2 <105132316+Landonscorner2@users.noreply.github.com>
2025-05-03 15:31:03 -0400

    Add files via upload

--- bias.sh
+++ bias.sh
@@ -2,14 +2,27 @@
 
 clear
 
-echo ".______    __       ___           _______.
+# Variables
+
+VERSION="0.1.0"
+
+LRED="\033[38;5;196m"
+END="\e[0m"
+LGREEN="\e[92m"
+BLUE="\e[34m"
+YELLOW="\e[93m"
+BOLD="\e[1m"
+
+BIASASCII=".______    __       ___           _______.
 |   _  \  |  |     /   \         /       |
 |  |_)  | |  |    /  ^  \       |   (----
 |   _  <  |  |   /  /_\  \       \   \    
 |  |_)  | |  |  /  _____  \  .----)   |   
 |______/  |__| /__/     \__\ |_______/ "
 
-echo "BIAS - Bias Isn't A Shell!"
+echo "${BIASASCII}"
+printf "BIAS v${VERSION} - Bias Isn't A Shell!\n"
+echo "Type 'help' to see a list of commands."
 
 printf "\n"
 
@@ -17,44 +30,59 @@ while true; do
 
         read -p ">: " userin
 
-        case $userin in
-                "help") echo "q, quit, exit, e - Exit BIAS"
-		        echo "f, files, dir - List files"
-			echo "rf - List root files"
-			echo "cf - Create file (with text)"
-			echo "ver, version - Current version of BIAS"
-			echo "c, cls, clr, clear - Clear screen"
-			echo "p, cat, print, read - Show words on a file"
-			echo "calc, biascalc, calculator - Built-in calculator"
-			echo "date, time - Show time and date"
-			echo "qc - Exit BIAS and clear screen"
-			echo "r, rm, delete, d - Remove file"	;;
+        case $userin in 
+		# Help
+		"help") printf "${LGREEN}System Commands:\n${END}"
+		echo "  q, quit, exit, e 	  - Exit BIAS"
+		echo "  c, cls, clr, clear	  - Clear screen"
+		echo "  ver, version		  - Current version of BIAS"
+		echo "  date, time 		  - Show date/time"
+		echo "  qc 		          - Clear and exit"
+		printf "${LGREEN}File Operations:\n${END}"
+		echo "  f, files, dir		  - List files"
+		echo "  rf                      - List root files"
+		echo "  cf			  - Create file"
+		echo "  p, cat, print		  - Read File"
+                echo "  r, rm, d, delete        - Remove File"
+		printf "${LGREEN}Tools:\n${END}"
+		echo "  calc, biascalc	  - Calculator"
+		echo "  weather		  - Show weather"
+	        echo "  sys, biasfetch    	  - System information"	;;
+                
+		# Exit BIAS
+                "q" | "quit" | "exit" | "e") echo "Exiting BIAS.."
+		exit ;;
+                
+		# List files
+                "f" | "files" | "dir" | "ls") ls ;;
 
-                "q" | "quit" | "exit" | "e") exit ;;
-
-                "f" | "files" | "dir") ls ;;
-
+		# List root files
                 "rf") ls / ;;
 
+		# Create files
                 "cf") read -p "Name?: " nameoffile
 		touch $nameoffile
 		read -p "Text?: " text
 		echo "$text" >> $nameoffile ;;
-
-                "ver" | "version") echo "BIAS 0.0.4" ;;
-
+                 
+		# Version 
+                "ver" | "version") echo "BIAS v${VERSION}" ;;
+                 
+		# Clear screen
                 "c" | "cls" | "clr" | "clear") clear ;;
 
+		# Print files
                 "p" | "cat" | "print" | "read") read -p "Print?: " printusertext
                 if [ "$printusertext" = "" ]; then
-			echo "Unable to print: Nothing inputted."
+			printf %b "${LRED}Unable to print: Nothing inputted.\n${END}"
 		else
 			printf "\n"
 			cat $printusertext
 			printf "\n"
 		fi	;;
 
-	        "calc" | "biascalc" | "calculator") echo "BiasCalc 0.0.4"
+		# Calculator
+	        "calc" | "biascalc") printf "${BOLD}BiasCalc v${VERSION}${END}\n"
 		printf "\n"
 		read -p "Operator? (+, *, -, /): " operator	
 		case $operator in
@@ -82,29 +110,43 @@ while true; do
 
 		esac	;;
 
+		# Time/date
 	        "date" | "time") date  ;;
 
-		"biasascii") echo ".______    __       ___           _______.
-|   _  \  |  |     /   \         /       |
-|  |_)  | |  |    /  ^  \       |   (----
-|   _  <  |  |   /  /_\  \       \   \    
-|  |_)  | |  |  /  _____  \  .----)   |   
-|______/  |__| /__/     \__\ |_______/"  ;;
+		"biasascii") echo "${BIASASCII}"  ;;
 
+		# Quit clear
 		"qc") clear
 		exit	;;
-
+                
+		# Remove file
 		"r" | "rm" | "delete" | "d") read -p "Delete?: " delfile
 		if [ "$delfile" = "" ]; then
-			echo "Unable to delete file: Nothing inputted."
+			printf "${LRED}Unable to delete file: Nothing inputted.\n${END}"
 		elif [ "$delfile" = "bias.sh" ]; then
-			echo "Unable to delete file: Deleting bias.sh will cause BIAS to be deleted."
+			printf "${LRED}Unable to delete file: Deleting bias.sh will cause BIAS to be deleted.\n${END}"
+		elif [ "$delfile" =  "/" ]; then
+			printf "${LRED}Unable to delete file: Deleting root will cause everything in the system to be deleted.\n${END}"
 		else
 			rm $delfile
 		fi ;;
+                
+		# Show weather
+		"weather") printf "Cities with two or more words in their name need a '_' to seperate them.\n"
+		read -p "City?: " weatherinput
+		echo "Fetching from wttr.in.." 
+		curl "http:/wttr.in/$weatherinput" ;;
 
+                # Fetch system info
+		"biasfetch" | "sys") printf "${BOLD}BiasFetch v${VERSION}${END}\n" 
+		printf "${BIASASCII}\n"
+		printf "${BLUE}BIAS Version:${END} ${VERSION}\n"
+		printf "${BLUE}Operating System:${END} $(uname -o)\n"
+		printf "${BLUE}Processor:${END} $(uname -p)\n"
+		printf "${BLUE}Machine:${END} $(uname -m)\n"
+		printf "${BLUE}Kernal:${END} $(uname -s)\n"
+		printf "${BLUE}Kernal Version:${END} $(uname -r)\n" ;;
 
-
-                *) echo "Unknown Command." ;;
+                *) echo "Unknown command: Not recognized by BIAS" ;;
         esac
 done           
